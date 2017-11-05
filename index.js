adfde7ce670014ce238f8e7a1451a1639236e175
Jeroen Nyckees <jenyckee@Jeroens-MacBook-Pro.local>
2017-11-05 11:34:47 +0100

    dancing line

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var group, camera, scene, renderer;
+const glslify = require('glslify');
+
+var group, camera, scene, renderer, pointsGeometry, controls;
 init();
 animate();
+window.addEventListener('click', randomize);
+// setInterval(randomize, 100);
 function init() {
     scene = new THREE.Scene();
     renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -9,13 +13,13 @@ function init() {
     document.body.appendChild( renderer.domElement );
     // camera
     camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.set( 15, 20, 30 );
+    camera.position.set( 0, 0, 75 );
     scene.add( camera );
     // controls
-    var controls = new THREE.OrbitControls( camera, renderer.domElement );
-    controls.minDistance = 20;
-    controls.maxDistance = 50;
-    controls.maxPolarAngle = Math.PI / 2;
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
+    // controls.minDistance = 20;
+    // controls.maxDistance = 50;
+    // controls.maxPolarAngle = Math.PI / 2;
     scene.add( new THREE.AmbientLight( 0x222222 ) );
     // light
     var light = new THREE.PointLight( 0xffffff, 1 );
@@ -28,9 +32,9 @@ function init() {
     group = new THREE.Group();
     scene.add( group );
     // points
-    var pointsGeometry = new THREE.DodecahedronGeometry( 10 );
+    pointsGeometry = new THREE.DodecahedronGeometry( 5 );
     for ( var i = 0; i < pointsGeometry.vertices.length; i ++ ) {
-        //pointsGeometry.vertices[ i ].add( randomPoint().multiplyScalar( 2 ) ); // wiggle the points
+        pointsGeometry.vertices[ i ].add( randomPoint().multiplyScalar( 2 ) ); // wiggle the points
     }
     var pointsMaterial = new THREE.PointsMaterial( {
         color: 0x0080ff,
@@ -43,14 +47,52 @@ function init() {
     // convex hull
     var meshMaterial = new THREE.MeshLambertMaterial( {
         color: 0xffffff,
-        opacity: 0.5,
+        opacity: 0.0,
         transparent: true
     } );
+    var lineMaterial = new THREE.LineBasicMaterial( {
+         color: 0xffffff,
+        //  linewidth: 40
+    });
+		const subdivisions = 300;
+		
+    const vert = glslify(__dirname + '/lib/shaders/tube.vert');
+    const frag = glslify(__dirname + '/lib/shaders/tube.frag');
+    const customLineMaterial = new THREE.RawShaderMaterial({
+			vertexShader: vert,
+			fragmentShader: frag,
+			side: THREE.FrontSide,
+			extensions: {
+					deriviatives: true
+			},
+			defines: {
+					lengthSegments: subdivisions.toFixed(1),
+					ROBUST: false,
+					ROBUST_NORMALS: true, // can be disabled for a slight optimization
+					FLAT_SHADED: true
+			},
+			uniforms: {
+					thickness: { type: 'f', value: 1 },
+					time: { type: 'f', value: 0 },
+					color: { type: 'c', value: new THREE.Color('#303030') },
+					animateRadius: { type: 'f', value: 10 },
+					animateStrength: { type: 'f', value: 10 },
+					index: { type: 'f', value: 0 },
+					totalMeshes: { type: 'f', value: 40 },
+					radialSegments: { type: 'f', value: 8 }
+			}
+			});
+    
+    var wireframe = new THREE.LineSegments( pointsGeometry, lineMaterial );
+    
+    scene.add( wireframe );
+
     var meshGeometry = new THREE.ConvexBufferGeometry( pointsGeometry.vertices );
     var mesh = new THREE.Mesh( meshGeometry, meshMaterial );
     mesh.material.side = THREE.BackSide; // back faces
     mesh.renderOrder = 0;
     group.add( mesh );
+
     var mesh = new THREE.Mesh( meshGeometry, meshMaterial.clone() );
     mesh.material.side = THREE.FrontSide; // front faces
     mesh.renderOrder = 1;
@@ -58,17 +100,37 @@ function init() {
     //
     window.addEventListener( 'resize', onWindowResize, false );
 }
+
+function randomize() {
+    for ( var i = 0; i < pointsGeometry.vertices.length; i ++ ) {
+        pointsGeometry.vertices[ i ] = ( randomPoint().multiplyScalar( 2 ) ); // wiggle the points
+    }
+    group.children.forEach(c => {
+        c.geometry.verticesNeedUpdate = true;
+    })
+}
+
+function ease() {
+    for ( var i = 0; i < pointsGeometry.vertices.length; i ++ ) {
+        pointsGeometry.vertices[ i ].add ( randomPoint().multiplyScalar( 2 ).divideScalar(100) ); // wiggle the points
+    }
+    group.children.forEach(c => {
+        c.geometry.verticesNeedUpdate = true;
+    })
+}
+
 function randomPoint() {
-    return new THREE.Vector3( THREE.Math.randFloat( - 1, 1 ), THREE.Math.randFloat( - 1, 1 ), THREE.Math.randFloat( - 1, 1 ) );
+    return new THREE.Vector3( THREE.Math.randFloat( - 5, 5 ), THREE.Math.randFloat( - 5, 5 ), THREE.Math.randFloat( - 1, 1 ) );
 }
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
-function animate() {
-    requestAnimationFrame( animate );
-    group.rotation.y += 0.005;
+function animate(i) {
+		requestAnimationFrame( animate );
+		controls.update();		
+    ease();
     render();
 }
 function render() {
